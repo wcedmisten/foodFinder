c42447ee293efeda95afceaa337ab799df963853
William Edmisten <wcedmisten@gmail.com>
2021-06-05 12:26:23 -0400

    Use docker image to parse ingredients from natural text

--- dedup.py
+++ dedup.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 import json
 
 new_dict = {}
 
 # all recipes retrieved from https://archive.org/download/recipes-en-201706/
-with open("/home/william/Downloads/allrecipes-recipes.json") as dataset:
-    with open("dup.json", "w") as dup:
+with open("/home/wedmisten/Downloads/allrecipes-recipes.json") as dataset:
+    with open("dedup.json", "w") as dup:
         for i, line in enumerate(dataset.readlines()):
             data = json.loads(line)
             # pprint.pprint(data)
--- test-scraper.py
+++ test-scraper.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 import json
 import pprint
+import subprocess
+import sys
 
 import nltk 
 import re 
@@ -9,7 +12,7 @@ import numpy as np
 def scrape_all_recipes():
     recipe_list = []
     
-    with open("dup.json") as dataset:
+    with open("dedup.json") as dataset:
         for i, line in enumerate(dataset.readlines()):
             data = json.loads(line)
 
@@ -22,17 +25,29 @@ def scrape_all_recipes():
             if i % 10000 == 0:
                 print(f"{i} / 91000")
 
+    return recipe_list
 
-def get_real_ingredients():
-    with open("real_ingredients.json") as real:
-        return json.load(real)
-    # with open("/home/william/Downloads/ingredients.json") as ingredients_data:
-    #     data = json.load(ingredients_data)
-    #     real_ingredients_list = [ingredient["name"].lower() for ingredient in data["tags"]]
 
-    #     return real_ingredients_list[:100]
+# def get_real_ingredients():
+#     with open("real_ingredients.json") as real:
+#         return json.load(real)
+#     with open("/home/william/Downloads/ingredients.json") as ingredients_data:
+#         data = json.load(ingredients_data)
+#         real_ingredients_list = [ingredient["name"].lower() for ingredient in data["tags"]]
+
+#         return real_ingredients_list[:100]
 
 # real_ingredients = get_real_ingredients()
 
+recipes = scrape_all_recipes()
+# only run on 10 to test with
+recipes = recipes[:10]
 
-scrape_all_recipes()
+for i, recipe in enumerate(recipes[:10]):
+    with open("input.txt", "w") as f:
+        f.write('\n'.join(recipe['ingredients']) + '\n')
+    
+    command = subprocess.run(['docker', 'run', '--mount', 'type=bind,source=/home/wedmisten/foodFinder/input.txt,target=/input/input.txt,readonly', 'wedmisten/ingredients-tagger'], capture_output=True)
+
+    sys.stdout.buffer.write(command.stdout)
+    sys.stderr.buffer.write(command.stderr)
